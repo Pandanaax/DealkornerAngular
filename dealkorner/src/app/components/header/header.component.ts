deb3099a6b55a17e5d97ab0e29473f7e10648513
Randa BAlI <baliranda@gmail.com>
2020-06-05 16:25:13 +0200

    module import

--- dealkorner/src/app/components/header/header.component.ts
+++ dealkorner/src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  registerForm: FormGroup;
+  submitted = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+   
+      this.registerForm = this.formBuilder.group({
+              email: ['', Validators.required],
+              password: ['', Validators.required],
+              
+      });
+    }
+    get f() { return this.registerForm.controls; }
+    
+    onSubmit() {
+        this.submitted= true;
+    
+        // stop here if form is invalid
+        if (this.registerForm.invalid) {
+            return;
+        }
+    
+        // display form values on success
+        alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
+    }
   }
 
-}
