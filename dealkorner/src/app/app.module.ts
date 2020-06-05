5e9d991c217f0edf00b0d25b21eca9f39000b7f5
Cedric Ktourza <cedric.ktourza@gmail.com>
2020-06-05 11:26:45 +0200

    CHANGEMent sur listdeal et viewdeal

--- dealkorner/src/app/app.module.ts
+++ dealkorner/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ListedealComponent } from './components/listedeal/listedeal.component';
 import { CreatedealComponent } from './components/createdeal/createdeal.component';
+import { ViewdealComponent } from './viewdeal/viewdeal.component';
 
 
 
@@ -15,7 +16,8 @@ import { CreatedealComponent } from './components/createdeal/createdeal.componen
     HeaderComponent,
     FooterComponent,
     ListedealComponent,
-    CreatedealComponent],
+    CreatedealComponent,
+    ViewdealComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
---
+++ dealkorner/src/app/viewdeal/viewdeal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-viewdeal',
+  templateUrl: './viewdeal.component.html',
+  styleUrls: ['./viewdeal.component.css']
+})
+export class ViewdealComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
