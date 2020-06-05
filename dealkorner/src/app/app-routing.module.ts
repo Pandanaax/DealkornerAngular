81e8620cd359807b3e3409ce731fc4b0d64a993e
Hicham <h.kassou91@gmail.com>
2020-06-05 14:27:44 +0200

    création du composant viewdeal

--- dealkorner/src/app/app-routing.module.ts
+++ dealkorner/src/app/app-routing.module.ts
@@ -2,14 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListedealComponent } from './components/listedeal/listedeal.component';
 import { CreatedealComponent } from './components/createdeal/createdeal.component';
-import { ViewdealComponent } from './components/viewdeal/viewdeal.component';
+
 
 
 
 const routes: Routes = [
   {path: '', component: ListedealComponent},
   {path: 'createdeal', component: CreatedealComponent},
-  {path: 'viewdeal', component: ViewdealComponent}
 ];
 
 @NgModule({
