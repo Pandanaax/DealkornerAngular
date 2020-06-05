2335c24cf9c6b7c4190ebdfb95773e82fee0812a
Cedric Ktourza <cedric.ktourza@gmail.com>
2020-06-05 14:48:49 +0200

    Rajout de la route viewdeal

--- dealkorner/src/app/app-routing.module.ts
+++ dealkorner/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListedealComponent } from './components/listedeal/listedeal.component';
 import { CreatedealComponent } from './components/createdeal/createdeal.component';
+import { ViewdealComponent } from './components/viewdeal/viewdeal.component';
 
 
 
@@ -9,6 +10,7 @@ import { CreatedealComponent } from './components/createdeal/createdeal.componen
 const routes: Routes = [
   {path: '', component: ListedealComponent},
   {path: 'createdeal', component: CreatedealComponent},
+  {path: 'viewdeal', component:ViewdealComponent}
 ];
 
 @NgModule({
--- dealkorner/src/app/components/viewdeal/viewdeal.component.ts
+++ dealkorner/src/app/components/viewdeal/viewdeal.component.ts
@@ -11,5 +11,4 @@ export class ViewdealComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
 }
